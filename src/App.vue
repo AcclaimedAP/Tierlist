f8b1838d302fb3cdd49d6ca471dd5d52c4c300d1
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-02-21 01:03:29 +0100

    added initial structure

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container"><h1>Tier list</h1>
+    <div class="placeholder"><h2>S+</h2></div>
+    <div class="placeholder"><h2>S</h2></div>
+    <div class="placeholder"><h2>A</h2></div>
+    <div class="placeholder"><h2>B</h2></div>
+    <div class="placeholder"><h2>C</h2></div>
+    <div class="placeholder"><h2>D</h2></div>
+  
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style scoped lang="scss">
+.container {
+  background-color: rgba(255, 255, 255, 0.1);
+  width: 80vw;
+  position: fixed;
+  top: 5vh;
+  left: 10vw;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  
+  h1 {
+    text-align: center;
+  }
+
+  .placeholder {
+    width: calc(100% - 40px);
+    margin: 20px;
+    height: 100px;
+    background-color: rgba(255, 255, 255, 0.1);
+    display: flex;
+
+    h2 {
+      height: 100%;
+      aspect-ratio: 1 / 1;
+      background-color: rgba(255, 255, 255, 0.1);
+      margin: 0;
+      text-align: center;
+      line-height: 100px;
+      font-size: 32px;
+    }
+  }
 }
 </style>
