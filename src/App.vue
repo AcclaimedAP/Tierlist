e721d06146f3b55acad4950e3f41007a63a79c1b
AcclaimedAP <alexander.w.pressfelt@gmail.com>
2023-02-21 01:46:45 +0100

    added makes components for every "tier"

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
-<script setup>
+<script>
+import tierContainer from './components/tierContainer.vue';
 
+export default {
+  data() {
+    return {
+      tiers: ['S+', 'S', 'A', 'B', 'C', 'D', 'Unranked']
+    }
+  },
+  components: {
+    tierContainer
+  }
+}
 </script>
 
 <template>
   <div class="container"><h1>Tier list</h1>
-    <div class="placeholder"><h2>S+</h2></div>
-    <div class="placeholder"><h2>S</h2></div>
-    <div class="placeholder"><h2>A</h2></div>
-    <div class="placeholder"><h2>B</h2></div>
-    <div class="placeholder"><h2>C</h2></div>
-    <div class="placeholder"><h2>D</h2></div>
+    <tierContainer v-for="tier in tiers" :key="tier.tiers" :tier="tier" />
   
   </div>
 </template>
@@ -24,27 +30,12 @@
   display: flex;
   flex-direction: column;
   flex-wrap: nowrap;
-  
+  height: 90vh;
+  overflow: scroll;
   h1 {
     text-align: center;
   }
 
-  .placeholder {
-    width: calc(100% - 40px);
-    margin: 20px;
-    height: 100px;
-    background-color: rgba(255, 255, 255, 0.1);
-    display: flex;
 
-    h2 {
-      height: 100%;
-      aspect-ratio: 1 / 1;
-      background-color: rgba(255, 255, 255, 0.1);
-      margin: 0;
-      text-align: center;
-      line-height: 100px;
-      font-size: 32px;
-    }
-  }
 }
 </style>
---
+++ src/components/tierContainer.vue
@@ -0,0 +1,69 @@
+<script>
+export default {
+    props: ['tier']
+}
+</script>
+
+<template>
+
+    <div class="tierContainer">
+        <h2>{{ tier }}</h2>
+        <div class="characterPortrait">
+            <div class="imageContainer">
+                <img src="https://api.genshin.dev/characters/qiqi/portrait" alt="placeholder image">
+            </div>
+            <h3>Qiqi</h3>
+        </div>
+        <div class="characterPortrait">
+            <div class="imageContainer">
+                <img src="https://api.genshin.dev/characters/qiqi/portrait" alt="placeholder image">
+            </div>
+            <h3>Qiqi</h3>
+        </div>
+        <div class="characterPortrait">
+            <div class="imageContainer">
+                <img src="https://api.genshin.dev/characters/qiqi/portrait" alt="placeholder image">
+            </div>
+            <h3>Qiqi</h3>
+        </div>
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+  .tierContainer {
+    width: calc(100% - 40px);
+    margin: 20px;
+    background-color: rgba(255, 255, 255, 0.1);
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    .characterPortrait {
+        background-color: rgba(255, 255, 255, 0.1);
+        margin: 10px;
+        .imageContainer {
+            max-height: 100px;
+            overflow: hidden;
+            img {
+                max-height: 200px;
+            }
+        }
+        h3 {
+            margin: 5px;
+
+        }
+    }
+
+
+    h2 {
+      height: 140px;
+      aspect-ratio: 1 / 1;
+      background-color: rgba(255, 255, 255, 0.1);
+      margin: 10px;
+      text-align: center;
+      line-height: 100px;
+      font-size: 32px;
+    }
+  }
+
+</style>
